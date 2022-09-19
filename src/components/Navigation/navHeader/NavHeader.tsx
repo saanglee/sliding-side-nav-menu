5aa0afe51a84561e8c73267a5df059581d9e442e
saanglee <sangllee20@gmail.com>
2022-09-19 23:38:08 +0900

    Feat : navigation header

--- src/components/Navigation/navHeader/NavHeader.tsx
+++ src/components/Navigation/navHeader/NavHeader.tsx
@@ -3,18 +3,41 @@ import logo from '../../../assets/logo.png';
 
 import cx from 'classnames';
 import styles from './navHeader.module.scss';
+import Icon from '../../Icon';
+import { useRecoilValue } from 'recoil';
+import { isDarkMode, isOpenMenu } from '../../../store/global';
 
 const NavHeader = () => {
+  const isOpen = useRecoilValue(isOpenMenu);
+  const isDark = useRecoilValue(isDarkMode);
   return (
-    <div className={styles.navHeader}>
+    <div className={cx(styles.navHeader, { [styles.openNavHeader]: isOpen })}>
       <div className={styles.titleWrapper}>
         <img src={logo} alt='logo' />
-        <div className={styles.title}>
+        <div className={cx(styles.title, { [styles.openTitle]: isOpen })}>
           <h2>I M Fine SD Team</h2>
           <p>Web Developer</p>
         </div>
       </div>
-      <input type='text' placeholder='Search... ' />
+
+      <div className={styles.inputWrapper}>
+        <div className={cx(styles.iconBox, { [styles.iconBoxDark]: isDark })}>
+          <span>
+            <Icon icon='SearchIcon' color={'#666666'} size='50px' />
+          </span>
+          <input
+            type='text'
+            placeholder='Search... '
+            className={cx(
+              styles.searchInput,
+              {
+                [styles.openSearchInput]: isOpen,
+              },
+              { [styles.inputDark]: isDark }
+            )}
+          />
+        </div>
+      </div>
     </div>
   );
 };
