7f43aaa3a2c622b4730d10eeddae96aadd079074
saanglee <sangllee20@gmail.com>
2022-09-20 14:08:19 +0900

    Feat : layout darkmode

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import Navigation from '../../components/Navigation';
+import { useRecoilValue } from 'recoil';
 import { Outlet } from 'react-router-dom';
+import { isDarkMode } from '../../store/global';
+import Navigation from '../../components/Navigation';
 import styles from './layout.module.scss';
+import cx from 'classnames';
 
 const Layout = () => {
+  const isDark = useRecoilValue(isDarkMode);
   return (
-    <div className={styles.layoutContainer}>
+    <div
+      className={cx(styles.layoutContainer, { [styles.layoutDark]: isDark })}
+    >
       <Navigation />
-      <main className={styles.layoutMain}>
+      <main>
         <Outlet />
       </main>
     </div>
